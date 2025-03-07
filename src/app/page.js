3786d47ba733362befbb108ac9e8c5895af8942e
Alex Pooley <zuedev@gmail.com>
2025-03-07 22:35:39 +0000

    migrate to nextjs

---
+++ src/app/page.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default () => {
+  const [countdown, setCountdown] = useState(9);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (countdown <= 1)
+        window.location.href = "https://www.twitch.tv/vtsweets";
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [countdown]);
+
+  return (
+    <>
+      <main className="h-screen flex items-center justify-center bg-linear-20 from-[var(--from)] via-[var(--via)] to-[var(--to)]">
+        <article className="transform-3d rotate-x-25 rotate-z-20 shadow-xl bg-black text-white p-4 rounded-lg cursor-pointer select-none text-xl font-bold max-w-md text-justify">
+          Welcome to <sweet>The Sweet Escape</sweet>! 🍬🎮
+          <br />
+          <br />
+          This page is still under construction.
+          <br />
+          <br />
+          Redirecting to my stream in {countdown}...
+        </article>
+      </main>
+    </>
+  );
+};
