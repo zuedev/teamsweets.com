ea6afc2b572d19b4f2a84d5fccd02cb6d669758d
Alex Pooley <zuedev@gmail.com>
2025-03-08 00:55:42 +0000

    add video preload link in layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata = {
   title: "Team Sweets",
@@ -11,6 +12,9 @@ export const metadata = {
 export default ({ children }) => {
   return (
     <html lang="en" className="bg-black text-white">
+      <Head>
+        <link rel="preload" href="/videos/laying_legacy.mp4" as="video" />
+      </Head>
       <body className="min-h-screen">{children}</body>
     </html>
   );
