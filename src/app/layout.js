a1fe5da93a9c88bb06fc86af8698559cd81c4394
Alex Pooley <zuedev@gmail.com>
2025-03-21 23:25:11 +0000

    wrap entire layout with suspense boundary

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Head from "next/head";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Team Sweets",
@@ -11,11 +12,13 @@ export const metadata = {
 
 export default ({ children }) => {
   return (
-    <html lang="en" className="bg-black text-white">
-      <Head>
-        <link rel="preload" href="/videos/laying_legacy.mp4" as="video" />
-      </Head>
-      <body className="min-h-screen">{children}</body>
-    </html>
+    <Suspense>
+      <html lang="en" className="bg-black text-white">
+        <Head>
+          <link rel="preload" href="/videos/laying_legacy.mp4" as="video" />
+        </Head>
+        <body className="min-h-screen">{children}</body>
+      </html>
+    </Suspense>
   );
 };
