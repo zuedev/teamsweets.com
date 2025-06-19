96413d62ed90543859ac97793c3f28e9db790284
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-06-19 15:33:49 +0100

    better landing animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,37 @@
 export default () => {
   return (
     <>
-      <video
-        playsInline
-        autoPlay
-        muted
-        loop
-        style={{
-          objectFit: "cover",
-          width: "100vw",
-          height: "100vh",
-          position: "fixed",
-          top: "0",
-          left: "0",
-          zIndex: "-1",
-        }}
-      >
-        <source type="video/mp4" src="/laying_legacy.mp4" />
-      </video>
+      <div className="animate-fade-in">
+        <video
+          playsInline
+          autoPlay
+          muted
+          loop
+          style={{
+            objectFit: "cover",
+            width: "100vw",
+            height: "100vh",
+            position: "fixed",
+            top: "0",
+            left: "0",
+            zIndex: "-1",
+          }}
+        >
+          <source type="video/mp4" src="/laying_legacy.mp4" />
+        </video>
 
-      <div>
-        <div className="min-h-screen flex flex-col items-center justify-center gap-8">
-          <h1 className="text-[12vw] font-bold leading-none sweets-text-gradient pt-[15px]">
-            Sweets
-          </h1>
-          <a
-            className="text-2xl px-6 py-2 bg-[#000] hover:bg-yellow-400 hover:text-black text-white transition-colors duration-500"
-            href="/card"
-          >
-            Enter the Sweet Escape
-          </a>
+        <div className="animate-fade-in-delayed-1s">
+          <div className="min-h-screen flex flex-col items-center justify-center gap-8">
+            <h1 className="text-[12vw] font-bold leading-none sweets-text-gradient pt-[15px]">
+              Sweets
+            </h1>
+            <a
+              className="text-2xl px-6 py-2 bg-[#000] hover:bg-yellow-400 hover:text-black text-white transition-colors duration-500"
+              href="/card"
+            >
+              Enter the Sweet Escape
+            </a>
+          </div>
         </div>
       </div>
     </>
